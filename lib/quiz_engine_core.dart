8b962a532803b9628f7cd4867e2b80dd7df8142d
Volodymyr Shyrochuk <vov4uks@gmail.com>
2025-03-15 18:48:12 +0200

    Updated imports

--- lib/quiz_engine_core.dart
+++ lib/quiz_engine_core.dart
@@ -2,6 +2,8 @@ library quiz_engine_core;
 
 export 'src/business_logic/quiz_bloc.dart';
 export 'src/business_logic/quiz_state/quiz_state.dart';
+export 'src/asset_provider.dart';
+export 'src/business_logic/quiz_data_provider.dart';
 export 'src/bloc/bloc_provider.dart';
 export 'src/model/answer.dart';
 export 'src/model/question.dart';
