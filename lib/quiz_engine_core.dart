003319100002bc0a73712c283251266d5d899ee3
Volodymyr Shyrochuk <vov4uks@gmail.com>
2025-03-15 17:25:57 +0200

    Added missing import

--- lib/quiz_engine_core.dart
+++ lib/quiz_engine_core.dart
@@ -2,5 +2,6 @@ library quiz_engine_core;
 
 export 'src/business_logic/quiz_bloc.dart';
 export 'src/business_logic/quiz_state/quiz_state.dart';
+export 'src/bloc/bloc_provider.dart';
 export 'src/model/answer.dart';
 export 'src/model/question.dart';
